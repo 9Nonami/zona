4906f524493ac771d125059606a517c737352c48
9Nonami <9nonami@gmail.com>
2020-11-26 21:03:49 -0300

    mais switch

--- switcher.lua
+++ switcher.lua
@@ -3,42 +3,85 @@ local switcher = {}
 -- * para este metodo, a sheet precisa estar toda na horizontal *
 -- shet: string do caminho da imagem com todos 'botoes' mais focus
 -- quadcoords: tabela com as coordenas de cada 'botao' NA sheet
---             { {10, 10}, {50, 99}, ... }
+--             { {x1, y2}, {x2, y2}, ... }
 -- qw: quad width
 -- qh: quad height
 -- total: quantidade de imagens NA sheet
-function createsheetandquads(sheet, quadcoords, qw, qh, total)
+function switcher.createsheetandquads(sheet, quadcoords, qw, qh, total)
+	local x = 1
+	local y = 2
 	switcher.sheet = love.graphics.newImage(sheet)
 	switcher.w = qw
 	switcher.h = qh
 	switcher.quads = {}
-	local x = 1
-	local y = 2
 	for i = 1, total do
 		table.insert(switcher.quads, love.graphics.newQuad(quadcoords[i][x], quadcoords[i][y], qw, qh, switcher.sheet:getDimensions()))
 	end
 end
 
-function createswitches(x, y, lvl)
+-- coordinates: tabela com as coordenadas para render e colisao
+--              { {x1, y1}, {x2, y2}, ... }
+function switcher.createswitches(coordinates)
+	local x = 1
+	local y = 2
+	local starterid = 1
+	switcher.switches = {}
+	for i = 1, #coordinates do
+		table.insert(switcher.switches, createswitch(coordinates[i][x], coordinates[i][y], starterid))
+	end
+end
+
+-- x: x no mapa
+-- y: y no mapa
+function switcher.createswitch(x, y)
 	local sw = {}
 	sw.x = x
 	sw.y = y
-	sw.lvl = 1
+	sw.id = 1
 	sw.on = false
 	return sw
 end
 
-return switcher
+-- max: o valor maximo que o switcher pode chegar. passando desse
+--      valor, retorna para 1
+function setmaxid(max)
+	switcher.max = max
+end
 
---[[
-function switcher.update(mx, my, ismouseclicked)
+-- mx: mouse x
+-- my: mouse y
+-- ismouseclicked: mouse clicked
+-- upgrades: dado externo,tabela que contem os limites que os switches podem alcancar
+--           antes de voltarem para 1
+function switcher.update(mx, my, ismouseclicked, upgrades)
 	local selected = -1
-	for i = 1, #switcher.squares do
-		if mx > switcher.squares[x] and mx < switcher.squares[x] + swidth and my > switcher.squares[y] and my < switcher.squares[y] + sheight then
+	for i = 1, #switcher.switches do
+		switcher.switches[i].on = mx > switcher.switches[i].x and mx < switcher.switches[i].x + switcher.w and my > switcher.switches[i].y and my < switcher.switches[i].y + switcher.h
+		if switcher.switches[i].on and ismouseclicked then
 			selected = i
 			goto done
 		end
 	end
 	::done::
+	if selected ~= -1 then
+		switcher.checkswitch(selected,  upgrades[selected])
+	end
 end
-]]
+
+-- selected: o id correstpondente ao switch selecionado dentro da tabela switches
+-- limit: dado exterior, vindo de upgrade, representa o maximo que o switch pode ir naquele momento (antes
+--        de voltar para 1)
+function switcher.checkswitch(selected, limit)
+	--[[
+	if switcher.switches[selected].id == switcher.max then
+		switcher.switches[selected].id = 1
+	elseif switcher.switches[selected].id < limit then
+		switcher.switches[selected].id = switcher.switches[selected].id + 1
+	end
+	]]
+	if switcher.switches[selected].id < limit then
+		switcher.switches[selected].id = switcher.switches[selecteed].id + 1
+	end
+end
+
+return switcher
